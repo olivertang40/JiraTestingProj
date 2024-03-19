8815e294d47c384e45e7a59ef97dd81197a49501
olivertang40 <olivertang224@gmail.com>
2024-03-19 17:39:32 -0500

    fix conflict (#18)
    Co-authored-by: petert40 <petertang224@gmail.com>

--- src/test/java/StepDefs/CreateSprintUI.java
+++ src/test/java/StepDefs/CreateSprintUI.java
@@ -1,80 +1,81 @@
-//package StepDefs;
-//
-//import Constants.TITLE;
-//import Constants.URL;
-//import Drivers.DriverFactory;
-//import PageObjectModel.HomePO;
-//import PageObjectModel.ProjectPO;
-//import io.cucumber.java.en.And;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.testng.Assert;
-//
-//public class SpringManageUI {
-//
-//    private final WebDriver driver;
-//    private HomePO homePO;
-//    private ProjectPO projectPO;
-//
-//    public SpringManageUI() {
-//        driver = DriverFactory.getDriver();
-//    }
-//
-//    @Given("I logged in as an admin with UI")
-//    public void iAmloggedAsAdmin() {
-//        homePO = new HomePO();
-//        projectPO = new ProjectPO();
-//        driver.navigate().to(URL.Home.toString());
-//
-//        // Enter username and password
-//        WebElement usernameInput = driver.findElement(By.id("login-form-username"));
-//        WebElement passwordInput = driver.findElement(By.id("login-form-password"));
-//        WebElement loginButton = driver.findElement(By.id("login"));
-//
-//        usernameInput.sendKeys("admin");
-//        passwordInput.sendKeys("12345");
-//
-//        // Click the login button
-//        loginButton.click();
-//    }
-//
-//    @When("I click projects button")
-//    public void iClickUserManagementButton() {
-//        homePO.clickProjectsBtn();
-//    }
-//
-//    @And("I click JIRATest project")
-//    public void iClickJIRATestProject(){
-//        homePO.clickJiraTestProject();
-//    }
-//
-//    @Then("I should view backlog page")
-//    public void iShouldViewBacklogPage() {
-//        Assert.assertEquals(driver.getTitle(),
-//                TITLE.JIR_BOARD.toString());
-//        projectPO.clickBacklogBtn();
-//    }
-//
-//
-//    @When("I click create a new Sprint")
-//    public void iClickCreateANewSprint() {
-//        projectPO.clickCreateSprint();
-//    }
-//
-//    @And("I click confirm create")
-//    public void iClickConfirmCreate() {
-//        projectPO.clickConfirm();
-//    }
-//
-//    @Then("The sprint should be created successfully with UI")
-//    public void theSprintShouldBeCreatedSuccessfullyWithUI() {
-//        WebElement sprintTitle = projectPO.sprintTitle;
-//        Assert.assertEquals(sprintTitle.getText(),"JIR Sprint 1");
-//    }
-//
-//
-//}
+package StepDefs;
+
+import Constants.TITLE;
+import Constants.URL;
+import Drivers.DriverFactory;
+import PageObjectModel.HomePO;
+import PageObjectModel.ProjectPO;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class CreateSprintUI {
+
+    private final WebDriver driver;
+    private HomePO homePO;
+    private ProjectPO projectPO;
+
+    public CreateSprintUI() {
+        driver = DriverFactory.getDriver();
+    }
+
+    @Given("I logged in as an admin with UI")
+    public void iAmloggedAsAdmin() {
+        homePO = new HomePO();
+        projectPO = new ProjectPO();
+        driver.navigate().to(URL.Home.toString());
+
+        // Enter username and password
+        WebElement usernameInput = driver.findElement(By.id("login-form-username"));
+        WebElement passwordInput = driver.findElement(By.id("login-form-password"));
+        WebElement loginButton = driver.findElement(By.id("login"));
+
+        usernameInput.sendKeys("admin");
+        passwordInput.sendKeys("admin12345");
+
+        // Click the login button
+        loginButton.click();
+    }
+
+    @When("I click projects button")
+    public void iClickUserManagementButton() {
+        homePO.clickProjectsBtn();
+    }
+
+    @And("I click JIRATest project")
+    public void iClickJIRATestProject(){
+        homePO.clickJiraTestProject();
+    }
+
+    @Then("I should view backlog page")
+    public void iShouldViewBacklogPage() {
+        Assert.assertEquals(driver.getTitle(),
+                TITLE.JIR_BOARD.toString());
+        projectPO.clickBacklogBtn();
+    }
+
+
+    @When("I click create a new Sprint")
+    public void iClickCreateANewSprint() {
+        projectPO.clickCreateSprint();
+    }
+
+    @And("I click confirm create")
+    public void iClickConfirmCreate() {
+        projectPO.clickConfirm();
+    }
+
+    @Then("The sprint should be created successfully with UI")
+    public void theSprintShouldBeCreatedSuccessfullyWithUI() {
+        WebElement sprintTitle = projectPO.sprintTitle;
+        Assert.assertEquals(sprintTitle.getText(),"JIR Sprint 1");
+    }
+
+
+
+}
