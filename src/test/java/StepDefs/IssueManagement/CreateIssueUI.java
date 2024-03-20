0bc4e33f944f89c9bf41a89ed2d7f3314cad8d6d
Zayn <wang.zee@northeastern.edu>
2024-03-20 02:08:29 -0400

    View My issue

--- src/test/java/StepDefs/IssueManagement/CreateIssueUI.java
+++ src/test/java/StepDefs/IssueManagement/CreateIssueUI.java
@@ -2,10 +2,8 @@ package StepDefs.IssueManagement;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Sleeper;
 
 public class CreateIssueUI {
@@ -16,9 +14,11 @@ public class CreateIssueUI {
         WebElement backLogButton = driver.findElement(By.xpath("//a[@aria-label='Backlog of test']"));
         backLogButton.click();
         // Find the button with the text "Create issue"
-//        WebElement createIssueButton = driver.findElement(By.xpath("//button[contains(text(), 'Create issue')]"));
+        if(driver.findElement(By.xpath("//button[contains(text(), 'Create issue')]")) != null){
+            WebElement createIssueButton = driver.findElement(By.xpath("//button[contains(text(), 'Create issue')]"));
+            createIssueButton.click();
+        }
         // Click on the button
-//        createIssueButton.click();
         Sleeper.SYSTEM_SLEEPER.sleep(java.time.Duration.ofSeconds(1));
         WebElement openInDialogButton = driver.findElement(By.xpath("//button[contains(text(), 'Open in dialog')]"));
         openInDialogButton.click();
@@ -32,6 +32,16 @@ public class CreateIssueUI {
         priority.sendKeys("high");
         Sleeper.SYSTEM_SLEEPER.sleep(java.time.Duration.ofSeconds(1));
         priority.sendKeys(Keys.ENTER);
+        Sleeper.SYSTEM_SLEEPER.sleep(java.time.Duration.ofSeconds(1));
+        // Find the dropdown menu
+        WebElement linkedIssue = driver.findElement(By.id("issuelinks-linktype"));
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].scrollIntoView(true);", linkedIssue);
+        // Create a new Select object
+        Sleeper.SYSTEM_SLEEPER.sleep(java.time.Duration.ofSeconds(1));
+        Select select = new Select(linkedIssue);
+        // Select the "blocks" option
+        select.selectByVisibleText("is blocked by");
         Sleeper.SYSTEM_SLEEPER.sleep(java.time.Duration.ofSeconds(1));
         WebElement createButton = driver.findElement(By.id("create-issue-submit"));
         createButton.click();
